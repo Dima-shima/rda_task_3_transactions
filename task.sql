571cdf85779c837d7ffb8776259a31d75cdc614e
Dima-shima <shifrin1970@gmail.com>
2026-06-14 09:35:25 +0200

    Edit task.sql

--- task.sql
+++ task.sql
@@ -1,14 +1,16 @@
 -- Use our database
 USE ShopDB; 
 
-INSERT INTO Orders (CustomerID, Date) VALUES (1, '2026-06-14');
-INSERT INTO OrderItems (OrderID, ProductID, Count) 
-VALUES (1, 1, 1);
 
 -- Start the transaction 
 START TRANSACTION; 
 
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 1
-WHERE ID = '1'; 
+INSERT INTO Orders (CustomerID, Date) VALUES (1, '2026-06-14');
+SET @order_id = LAST_INSERT_ID();
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@order_id, 1, 1);
+SET @qty = 1;
+UPDATE Products SET WarehouseAmount = WarehouseAmount - @qty
+WHERE ID = 1; 
 
 COMMIT; 
