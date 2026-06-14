8096f4507e5c81952181e4accc7db05d9a89c84d
Dima-shima <shifrin1970@gmail.com>
2026-06-14 10:14:21 +0200

    Edit task.sql

--- task.sql
+++ task.sql
@@ -1,12 +1,12 @@
 -- Use our database
 USE ShopDB; 
 
+INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-01');
+SET @order_id = LAST_INSERT_ID();
 
 -- Start the transaction 
 START TRANSACTION; 
 
-INSERT INTO Orders (CustomerID, Date) VALUES (1, '2026-06-14');
-SET @order_id = LAST_INSERT_ID();
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (@order_id, 1, 1);
 SET @qty = 1;
